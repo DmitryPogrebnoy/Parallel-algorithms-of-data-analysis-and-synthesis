e2e0a39ca3f9c3abd9b91395c171c89acbeda915
Dmitry Pogrebnoy <pogrebnoy.inc@gmail.com>
2021-12-17 00:46:17 +0700

    Add tasks 0, 1, 2

---
+++ OmpTasks/Task0/Task0.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    string const delims{ " \t\n" };
+    string inputString = argv[1];
+    size_t begin, pos = 0;
+    int counter = 0;
+    while ((begin = inputString.find_first_not_of(delims, pos)) != std::string::npos)
+    {
+        pos = inputString.find_first_of(delims, begin + 1);
+        counter++;
+    }
+    cout << "Input string contains " << counter << " words" << endl;
+}
---
+++ OmpTasks/Task1/Task1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <omp.h>
+
+using namespace std;
+
+long const N = 100000000;
+
+double max(int A[], int i, int j, int threadNumber)
+{
+    double t1 = omp_get_wtime();
+    int max_val = A[0];
+
+    #pragma omp parallel for reduction(max:max_val) num_threads(threadNumber)
+    for (int idx = i; idx < j; idx++)
+        max_val = max_val > A[idx] ? max_val : A[idx];
+
+    if (j != N) {
+        cout << "Max value is " << max_val << endl;
+    }
+    return omp_get_wtime() - t1;
+}
+
+int main(int argc, char* argv[])
+{
+    cout.precision(10);
+    int B[10] = { 1,2,3,4,5,6,7,8,9,10 };
+    cout << "Test run for array with 10 elements - " << max(B, 0, 10, 8) << endl << endl;
+
+    for (int i = 1; i < 11; i++) {
+        int* A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = rand();
+        }
+        int K = 20;
+        double time = 0;
+        for (int j = 0; j < K; j++) {
+            time += max(A, 0, N, i);
+        }
+        cout.precision(10);
+        cout << "Run with " << i << " threads takes average time - " << time/K << endl << endl;
+        delete[] A;
+    }
+}
