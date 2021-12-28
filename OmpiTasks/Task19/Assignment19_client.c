6cfaaa38c37b85ca657b33f06b1f66fbde5859d8
Dmitry Pogrebnoy <dmitry.pogrebnoy@jetbrains.com>
2021-12-28 18:20:04 +0300

    Fix task 19

--- OmpiTasks/Task19/Assignment19_client.c
+++ OmpiTasks/Task19/Assignment19_client.c
@@ -5,14 +5,22 @@ int main(int argc, char **argv)
 {
 	MPI_Init(&argc, &argv);
 	char port_name[MPI_MAX_PORT_NAME];
+	MPI_Status status;
 	MPI_Comm intercomm;
 	// Get port_name from argv
 	strcpy(port_name, argv[1]);
 	// Connect to server using port_name
+	printf("Attemp to connect\n");
 	MPI_Comm_connect(port_name, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm);
+	printf("Client connected\n");
 	// Send message to server
-	char message[11] = "Hi server!";
-	MPI_Send(message, 11, MPI_CHAR, 0, 0, intercomm);
+	int message = 42;
+	MPI_Send(&message, 1, MPI_INT, 0, 0, intercomm);
+	printf("Client sent value: %d\n", message);
+	// Receive message from server
+	int recv_message;
+	MPI_Recv(&recv_message, 1, MPI_INT, 0, 0, intercomm, &status); 
+	printf("Client got value: %d\n", recv_message);
 	MPI_Finalize();
 	return 0;
 }
--- OmpiTasks/Task19/Assignment19_serv.c
+++ OmpiTasks/Task19/Assignment19_serv.c
@@ -8,13 +8,18 @@ int main(int argc, char **argv)
 	MPI_Status status;
 	MPI_Comm intercomm;
 	MPI_Open_port(MPI_INFO_NULL, port_name);
-	printf("portname: %s\n", port_name);
+	printf("Portname: %s\n", port_name);
+	printf("Waiting for the client...\n");
 	MPI_Comm_accept(port_name, MPI_INFO_NULL, 0, MPI_COMM_SELF, &intercomm);
-	char message[11];
-	MPI_Recv(message, 11, MPI_CHAR, 0, 0, intercomm, &status);
+	printf("Server connected\n");
+	int message = 25;
+	MPI_Send(&message, 1, MPI_INT, 0, 0, intercomm);
+	printf("Server sent value: %d\n", message);
+	int recv_message;
+	MPI_Recv(&recv_message, 1, MPI_INT, 0, 0, intercomm, &status);
+	printf("Server got value: %d\n", recv_message);
 	MPI_Comm_free(&intercomm);
 	MPI_Close_port(port_name);
-	printf("Клиент отправил `%.11s`\n", message);
 	MPI_Finalize();
 	return 0;
 }
